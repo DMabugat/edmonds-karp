421a291010bbd38aef7b24db059ddac84b334e47
Daniel Mabugat <daniel.mabugat.937@my.csun.edu>
2017-12-17 20:38:30 -0800

    Update source.java

--- src/source.java
+++ src/source.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 Daniel Mabugat
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php
+
 import Graph.FlowNetwork;
 
 public class source {
